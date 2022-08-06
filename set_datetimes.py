e6a6b68a39bd00c00dd212391e7c1f76f22f56e6
Mateusz Soszyński <mateusz.soszynski@tuta.io>
2022-08-06 16:44:42 +0200

    Why this was like that??

--- set_datetimes.py
+++ set_datetimes.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 import os
-from pathlib import Path
-
-
-def set_datetimes(datetimes_map: dict[str, int], folder: Path) -> None:
-    for filename in datetimes_map:
-        file = folder / filename
-        os.utime(file, (datetimes_map[filename], datetimes_map[filename]))
-
 
 if __name__ == '__main__':
     import json
 
     with open('datetimes.json', 'r') as f:
-        set_datetimes(json.load(f), Path('DCIM/Camera'))
+        dates_map = json.load(f)
+        for filename in dates_map:
+            os.utime(filename, (dates_map[filename], dates_map[filename]))
